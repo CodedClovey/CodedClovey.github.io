5d0113bb99db0149f09938be3da6bb53c5153d96
Abhinav Manoj <abhinamanoj222@gmail.com>
2023-12-10 18:19:13 +0530

    sounds

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function showname(x) {
     var classname = x.className
     const element = document.activeElement.tagName;
     document.getElementById("nametag").innerHTML = "<h1>"+classname+"</h1>";
+    document.getElementById('myaudio').play()
 }
 
 function cut() {
