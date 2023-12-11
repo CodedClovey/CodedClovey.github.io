2c3bd1eb5fffcc78a58e8b03d268265798c41d8d
Abhinav Manoj <abhinamanoj222@gmail.com>
2023-12-11 19:24:23 +0530

    bg circle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function showname(x) {
     var classname = x.className
     const element = document.activeElement.tagName;
@@ -9,3 +10,15 @@ function cut() {
     document.getElementById("nametag").innerHTML = " ";
 }
 
+var $circle = $('.circle');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 10, {
+    css: {
+      left: e.pageX - 250,
+      top: e.pageY - 250
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
